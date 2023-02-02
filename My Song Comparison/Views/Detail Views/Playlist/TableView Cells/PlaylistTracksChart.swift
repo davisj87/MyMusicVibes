df44377fe89db7bbc03a8fe923adc3149efcec3a
Jarred <Jarred.davis87@gmail.com>
2023-02-02 16:20:09 -0500

    updates to UI of playlist track chart

--- My Song Comparison/Views/Detail Views/Playlist/TableView Cells/PlaylistTracksChart.swift
+++ My Song Comparison/Views/Detail Views/Playlist/TableView Cells/PlaylistTracksChart.swift
@@ -13,26 +13,44 @@ struct PlaylistTracksChart: View {
     var body: some View {
         if #available(iOS 16.0, *) {
             VStack {
-                GroupBox ( "Musicial Positivity") {
+                GroupBox ("Musicial Positivity") {
                     Chart {
                         ForEach(self.vm.musicalPositivityArr) {
                             LineMark(
-                                x: .value("Positivity", $0.positivity),//.value("Positivity", $0.score)
+                                x: .value("Positivity", $0.positivity),
                                 y: .value("# of Tracks", $0.numTracks)
                             )
+                            .lineStyle(.init(lineWidth: 10, lineCap: .round, lineJoin: .round))
                         }
+                        .foregroundStyle(Gradient(colors: [.pink, .orange, .yellow]))
+                        .interpolationMethod(.monotone)
+                    }
+                    //.frame(height:300)
+                    .chartYAxis {
+                        AxisMarks(position: .leading)
+                    }
+                    .chartYAxisLabel(position: .leading, alignment: .center) {
+                        Text("# of Tracks")
                     }
                 }
                 
-//                GroupBox ( "Line Chart - Step Count") {
-//                    Chart(currentWeek) {
-//                        LineMark(
-//                            x: .value("Week Day", $0.weekday, unit: .day),
-//                            y: .value("Step Count", $0.steps)
-//                        )
-//
-//                    }
-//                }
+                GroupBox ("Key") {
+                    Chart {
+                        ForEach(self.vm.musicalPositivityArr) {
+                            LineMark(
+                                x: .value("Positivity", $0.positivity),
+                                y: .value("# of Tracks", $0.numTracks)
+                            )
+                            .lineStyle(.init(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                        }
+                        .foregroundStyle(Gradient(colors: [.pink, .orange, .yellow]))
+                        .interpolationMethod(.monotone)
+                    }
+                   // .frame(height:300)
+                    .chartYAxis {
+                        AxisMarks(position: .leading)
+                    }
+                }
             }
         } else {
             // Fallback on earlier versions
