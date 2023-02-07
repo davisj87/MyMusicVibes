bbbaa4c6d47b58123157d13e9e2a8af848037247
Jarred <Jarred.davis87@gmail.com>
2023-02-07 09:53:24 -0500

    Added null value handling in Playlist Model

--- My Song Comparison/Models/Playlist/PlaylistObject.swift
+++ My Song Comparison/Models/Playlist/PlaylistObject.swift
@@ -7,10 +7,25 @@
 
 import Foundation
 struct PlaylistObject: Decodable {
-    var name: String
     var id: String
+    var name: String
     var owner: PlaylistOwnerObject
     var images: [ImageObject]
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, name, owner, images
+    }
+    
+}
+
+extension PlaylistObject {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decodeIfPresent(String.self, forKey: .id) ?? ""
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? "N/A"
+        self.owner = try container.decodeIfPresent(PlaylistOwnerObject.self, forKey: .owner) ?? PlaylistOwnerObject(name: "N/A")
+        self.images = try container.decodeIfPresent([ImageObject].self, forKey: .images) ?? []
+    }
 }
 
 struct PlaylistOwnerObject: Decodable {
@@ -18,5 +33,12 @@ struct PlaylistOwnerObject: Decodable {
     
     private enum CodingKeys: String, CodingKey {
         case name = "display_name"
+    }
+}
+
+extension PlaylistOwnerObject {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? "N/A"
     }
 }
--- My Song Comparison/Models/Playlist/PlaylistTrackObject.swift
+++ My Song Comparison/Models/Playlist/PlaylistTrackObject.swift
@@ -9,4 +9,21 @@ import Foundation
 
 struct PlaylistTrackObject: Codable {
     var track: TracksObject
+    
+    private enum CodingKeys: String, CodingKey {
+        case track
+    }
+}
+
+
+extension PlaylistTrackObject {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let emptyTrackAlbumObject = TrackAlbumObject(images: [], name: "N/A")
+        self.track = try container.decodeIfPresent(TracksObject.self, forKey: .track) ?? TracksObject(id: "",
+                                                                                                      name: "N/A",
+                                                                                                      popularity: -1,
+                                                                                                      album: emptyTrackAlbumObject,
+                                                                                                      artists: [])
+    }
 }
